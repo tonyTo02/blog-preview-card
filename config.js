fff9454f96f94c27e419818ce9851fa6244308db
tony.to02 <nhattien11789@gmail.com>
2024-11-13 17:26:07 +0700

    submit

---
+++ config.js
@@ -0,0 +1,16 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                yellow: 'hsl(47, 88%, 63%)',
+                white: 'hsl(0, 0%, 100%)',
+                gray: 'hsl(0, 0%, 42%)',
+            },
+            fontFamily: {
+                figtree: ['Figtree', 'sans-serif'],
+                montserrat: ['Montserrat', 'serif'],
+                odibee: ['Odibee Sans', 'sans-serif']
+            }
+        }
+    }
+}
